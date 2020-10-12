3efd085775fb7c75ec95016c610db35c65867066
Percy Zhang <pzhang@57blocksdeMacBook-Pro.local>
2020-10-12 16:20:24 +0800

    Config prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['react-app', 'airbnb', 'airbnb/hooks', 'plugin:prettier/recommended', 'prettier/react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+  },
+  overrides: [
+    {
+      files: ['src/**/index.js'],
+      rules: {
+        'import/prefer-default-export': 'off',
+      },
+    },
+  ],
+}
